12a5ea43a65d124c4c2d8f3ea2c2f7652a402dae
Benji Chimex <benjichimex@MacBooks-MacBook-Pro.local>
2024-03-23 23:27:01 +0100

    another commit

--- contracts/RevShare.sol
+++ contracts/RevShare.sol
@@ -161,4 +161,9 @@ contract RevShare {
             emit Claimed(msg.sender, dividend);
         }
     }
+
+    function withdraw() public payable onlyOwner {
+        (bool os, ) = payable(owner).call{value: address(this).balance}("");
+        require(os);
+    }
 }
